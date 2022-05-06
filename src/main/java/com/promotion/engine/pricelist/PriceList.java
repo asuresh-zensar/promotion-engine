52255e81b35794ac79040afa4e4fb70bfb124434
Arikatla Suresh <ASuresh@Zensar.com>
2022-05-06 10:57:42 +0530

    Added Price for the SKUs

--- src/main/java/com/promotion/engine/pricelist/PriceList.java
+++ src/main/java/com/promotion/engine/pricelist/PriceList.java
@@ -1,5 +1,20 @@
 package com.promotion.engine.pricelist;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PriceList {
 
+	private static Map<String, Double> skus = new HashMap<>();
+
+    static {
+    	skus.put("A", 50.0);
+    	skus.put("B", 30.0);
+    	skus.put("C", 20.0);
+    	skus.put("D", 15.0);
+    }
+
+    public static Double getPrice(String skuId) {
+        return skus.get(skuId);
+    }
 }
