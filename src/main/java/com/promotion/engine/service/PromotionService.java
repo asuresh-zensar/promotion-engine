ab1229c690dc1f91d385d33f9522850bec8f5993
Arikatla Suresh <ASuresh@Zensar.com>
2022-05-06 11:29:33 +0530

    Created Promotion service class with calculateOrderTotal prototype

---
+++ src/main/java/com/promotion/engine/service/PromotionService.java
@@ -0,0 +1,13 @@
+package com.promotion.engine.service;
+
+import com.promotion.engine.model.Cart;
+
+public class PromotionService {
+
+	public double calculateOrderTotal(Cart cart) {
+		
+		double total=0.0;
+		
+		return total;
+	}
+}
